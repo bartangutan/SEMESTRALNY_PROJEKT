53bd1e00a5f8462209262abb88e336e99621406e
bartangutan <19bartosz11@gmail.com>
2026-02-05 12:39:45 +0100

    randomowa_cyfra

--- app/src/main/java/com/example/projekt_na_semestr/MainActivity.java
+++ app/src/main/java/com/example/projekt_na_semestr/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.projekt_na_semestr;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
+import java.util.SortedMap;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +25,26 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.buttonSTART);
+        textView = findViewById(R.id.textView1);
+        //textView.setText(""+sekwencja());
+
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        textView.setText(""+sekwencja());
+                    }
+                }
+        );
+
+
     }
 
-    public void sekwencja(){
+    public int sekwencja(){
         Random random = new Random();
-        random.nextInt();
+        int liczba = random.nextInt(1,5 );
+        return  liczba;
+        //dodawać to do arraylisty, przechodzić przez listę i podświetlać konkretne buttony
     }
 }
